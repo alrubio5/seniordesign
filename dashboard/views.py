b518a2d93de1dc84874c3397ed7dd2f034e3380e
alrubio5 <79871252+alrubio5@users.noreply.github.com>
2021-03-07 16:30:25 -0600

    Add files via upload

--- dashboard/views.py
+++ dashboard/views.py
@@ -8,21 +8,67 @@ import plotly.express as px
 
 x_data = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
 y_data = [x**0.5 for x in x_data]
-plot_div = plot([Scatter(x=x_data, y=y_data,
+plot_div1 = plot([Scatter(x=x_data, y=y_data,
 					mode='lines', name='test',
 					opacity=0.8, marker_color='green')],
+			output_type='div')
+plot_div2 = plot([Scatter(x=y_data, y=x_data,
+					mode='lines', name='test2',
+					opacity=0.8, marker_color='blue')],
 			output_type='div')
 
 def index(request, pagename):
 	pagename = '/' + pagename
 	pg = page.objects.get(permalink=pagename)
-	context = {
-		'title': pg.title,
-		'content': pg.bodytext,
-		'last_updated': pg.update_date,
-		'page_list': page.objects.all(),
-		'plot_div': plot_div
-
-	}
+	if pagename=='/':
+		context = {
+			'title': pg.title,
+			'content': pg.bodytext,
+			'last_updated': pg.update_date,
+			'page_list': page.objects.all(),
+			}
+	if pagename=='/about':
+		context = {
+			'title': pg.title,
+			'content': pg.bodytext,
+			'last_updated': pg.update_date,
+			'page_list': page.objects.all(),
+			}
+	if pagename=='/coldwatert':
+		context = {
+			'title': pg.title,
+			'content': pg.bodytext,
+			'last_updated': pg.update_date,
+			'page_list': page.objects.all(),
+			}
+	if pagename=='/coldwaterp':
+		context = {
+			'title': pg.title,
+			'content': pg.bodytext,
+			'last_updated': pg.update_date,
+			'page_list': page.objects.all(),
+			'plot_div': plot_div1,
+			}
+	if pagename=='/utilization':
+		context = {
+			'title': pg.title,
+			'content': pg.bodytext,
+			'last_updated': pg.update_date,
+			'page_list': page.objects.all(),
+			}
+	if pagename=='/machinevibe':
+		context = {
+			'title': pg.title,
+			'content': pg.bodytext,
+			'last_updated': pg.update_date,
+			'page_list': page.objects.all(),
+			}
+	if pagename=='/enclosetemp':
+		context = {
+			'title': pg.title,
+			'content': pg.bodytext,
+			'last_updated': pg.update_date,
+			'page_list': page.objects.all(),
+			'plot_div': plot_div2,
+			}
 	return render(request, 'dashboard/page.html', context)
-
